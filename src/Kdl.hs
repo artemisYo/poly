adab159f17769b02bd6f3edb343ecd906b242e96
artemisYo <art06and@gmail.com>
2024-03-17 19:55:13 +0100

    Quick save commit: I'm deling Kdl.hs

--- src/Kdl.hs
+++ src/Kdl.hs
@@ -1,19 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Kdl where
 
 import qualified Data.Map.Strict as Map
-import Control.Applicative (Alternative, empty, (<|>))
-import Data.Bifunctor (Bifunctor, bimap)
-import Data.List (isPrefixOf, length)
+import qualified Data.Text as Text
+import Data.Text (Text)
+import Control.Applicative (Alternative, empty, (<|>), some, many)
 import Control.Monad (MonadPlus)
-import Data.Function ((&))
-import Data.Tuple (swap)
+import Data.Foldable (find)
 
 type Document = [Node]
 
-type Ident = String
+type Ident = Text
 
 data Value
-  = Str String
+  = Str Text
   | Num Double
   | Bool Bool
   | Null
@@ -25,8 +26,70 @@ data Node = Node
   , children :: [Node]
   }
 
-parse_doc :: String -> Document
+parse_doc :: Text -> Document
 parse_doc _ = []
+
+voidP :: (Monad m) => Parser i m o -> Parser i m ()
+voidP = fmap void
+void :: a -> ()
+void _ = ()
+
+linespace :: Parser Text Maybe ()
+linespace = voidP newlineP <|> ws <|> singleLineComment
+
+newlineP :: Parser Text Maybe Char
+newlineP = Parser $ startsPred newline
+newline :: Char -> Bool
+newline = flip elem
+  [ '\x000d', '\x000a' -- CRLF is skipped as that only changes how
+  , '\x0085', '\x000c' --  many newlines it produces and idc that
+  , '\x2028', '\x2029'
+  ]
+
+ws :: Parser Text Maybe ()
+ws = bom <|> unicodeSpace <|> multiLineComment
+
+singleLineComment :: Parser Text Maybe ()
+singleLineComment = voidP $ start *> middle *> end
+  where
+    start = Parser $ startsText "//"
+    middle = some $ Parser $ startsPred $ not . newline
+    end = newlineP <|> eof
+
+multiLineComment :: Parser Text Maybe ()
+multiLineComment = start *> commentedBlock
+  where
+    start = Parser $ startsText "/*"
+
+commentedBlock :: Parser Text Maybe ()
+commentedBlock = end <|> (nest *> commentedBlock)
+  where
+    nest  = multiLineComment <|> star <|> slash <|> rest
+    rest  = voidP $ some $ Parser $ startsPreds notEnd
+    star  = voidP $ Parser $ startsChar '*'
+    slash = voidP $ Parser $ startsChar '/'
+    end   = voidP $ Parser $ startsText "*/"
+    notEnd t = if not $ Text.isPrefixOf "*/" t
+      then Just 1
+      else Nothing -- this might be inefficient as fuck
+
+bom :: Parser Text Maybe ()
+bom = voidP $ Parser $ startsChar '\xfeff'
+
+unicodeSpace :: Parser Text Maybe ()
+unicodeSpace = voidP $ Parser $ startsPred $ flip elem
+  [ '\x0009', '\x0020'
+  , '\x00a0', '\x1680'
+  , '\x2000', '\x2001'
+  , '\x2002', '\x2003'
+  , '\x2004', '\x2005'
+  , '\x2006', '\x2007'
+  , '\x2008', '\x2009'
+  , '\x200a', '\x202f'
+  , '\x205f', '\x3000'
+  ]
+
+eof = fail ""
 
 -- | Right to left function composition, allowing the right function to be applied twice
 infixr 9 .:
@@ -76,13 +139,23 @@ instance (Monad m, Alternative m) => Alternative (Parser i m) where
 
 instance (Monad m, Alternative m) => MonadPlus (Parser i m) where
 
-startsChar :: Char -> String -> Maybe (String, Char)
-startsChar _ [] = Nothing
-startsChar c (x:xs)
-  | x == c = Just (xs, c)
-  | otherwise = Nothing
+-- instance NatTrans (Parser i _ o) where
+ntrans :: (Monad m, Monad n) => (m (i, o) -> n (i, o)) -> Parser i m o -> Parser i n o
+ntrans f p = Parser $ \i ->  f $ run p i
 
-startsString :: String -> String -> Maybe (String, String)
-startsString a b = if a `isPrefixOf` b
-  then Just (a, drop (length a) b)
-  else Nothing
+-- | represents a natural transformation: a morphism over functors
+infixl 4 ~>
+(~>) :: (Monad m, Monad n) => (m (i, o) -> n (i, o)) -> Parser i m o -> Parser i n o
+(~>) = ntrans
+
+startsChar :: Char -> Text -> Maybe (Text, Char)
+startsChar c = find ((c ==) . snd) . Text.unsnoc
+
+startsText :: Text -> Text -> Maybe (Text, Text)
+startsText a = fmap (a,) . Text.stripPrefix a
+
+startsPred :: (Char -> Bool) -> Text -> Maybe (Text, Char)
+startsPred p = find (p . snd) . Text.unsnoc
+
+startsPreds :: (Text -> Maybe Int) -> Text -> Maybe (Text, Text)
+startsPreds p t = flip Text.splitAt t <$> p t
